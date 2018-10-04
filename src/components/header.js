4eba0654ce6df47fd84ecece3573d5b7b15a2773
Tyron Wytrykowski <tyronwyt@gmail.com>
2018-10-04 15:25:41 +0100

    make nav mobile first with media queries

--- src/components/header.js
+++ src/components/header.js
@@ -7,14 +7,18 @@ import logo from "../assets/images/HabitusCapital_Logo.svg"
 const Header = () => (
     <header className={headerStyles.header}>
         <div className={headerStyles.wrapper}>
-            <a href="/" className={headerStyles.logo}><img src={logo} alt="Habitus Capital" /></a>
-            <ul>
-                <li><a href="#">Our Legacy</a></li>
-                <li><a href="#">Investing</a></li>
-                <li><a href="#">Performance</a></li>
-                <li><a href="#">Live Updates</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
+            <div className={headerStyles.logo}>
+                <a href="/" className={headerStyles.logo}><img src={logo} alt="Habitus Capital" /></a>
+            </div>
+            <div className={headerStyles.nav}>
+                <ul>
+                    <li><a href="#">Our Legacy</a></li>
+                    <li><a href="#">Investing</a></li>
+                    <li><a href="#">Performance</a></li>
+                    <li><a href="#">Live Updates</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
         </div>
     </header>
 )
