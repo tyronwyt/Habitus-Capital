e2f9e9bee7a8f1e69d37efa6e623a7b48ae78f82
Tyron Wytrykowski <tyronwyt@gmail.com>
2018-10-05 16:21:43 +0100

    add gatsby-source-filesystem and markdown transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,13 @@ module.exports = {
     plugins: [
         `gatsby-plugin-netlify-cms`,
         `gatsby-plugin-sass`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+              name: `src`,
+              path: `${__dirname}/src/`,
+            },
+        },
+        `gatsby-transformer-remark`,
     ],
 }
