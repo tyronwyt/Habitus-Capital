72b1ba8a7127556a38f1c46121165cc16c47abfb
Tyron Wytrykowski <tyronwyt@gmail.com>
2018-10-09 14:22:56 +0100

    add favicons

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,9 @@ const Layout = ({children}) => (
         <Helmet>
                 <meta charSet="utf-8" />
                 <title>Habitus Capital</title>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
             </Helmet>
         <Header/>
         {children}
