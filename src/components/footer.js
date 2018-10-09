2f81c5ba031e8234e61c66b6978da72f88278797
Tyron Wytrykowski <tyronwyt@gmail.com>
2018-10-09 17:36:40 +0100

    add social icons to footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,9 @@ import { StaticQuery, graphql } from "gatsby"
 
 import footerStyles from "./footer.module.scss"
 import logo from "../assets/images/HabitusCapital_Logo.svg"
+import twitter from "../assets/images/twitter_w.svg"
+import youtube from "../assets/images/youtube_w.svg"
+import linkedin from "../assets/images/linkedin_w.svg"
 
 
 const year = new Date().getFullYear();
@@ -25,6 +28,17 @@ const Footer = () => (
         <footer className={footerStyles.footer}>
             <div className={footerStyles.wrapper}>
                 <div className={footerStyles.blurb}>
+                    <div className={footerStyles.social}>
+                        <a href="https://twitter.com/HabitusCapital">
+                            <img src={twitter} alt="Twitter"/>
+                        </a>
+                        <a href="#">
+                            <img src={youtube} alt="Youtube"/>
+                        </a>
+                        <a href="#">
+                            <img src={linkedin} alt="Linkedin"/>
+                        </a>
+                    </div>
                     <p><strong>Address:</strong> {data.site.siteMetadata.address}<br/>
                     <strong>Email:</strong> {data.site.siteMetadata.email}<br/>
                     <strong>Telephone:</strong> {data.site.siteMetadata.telephone}</p>
