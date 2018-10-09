2bde3ca0e88a825eac8789fba3ea3ddaedf2a258
Tyron Wytrykowski <tyronwyt@gmail.com>
2018-10-09 12:15:54 +0100

    install react-helmet for head information

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     plugins: [
         `gatsby-plugin-netlify-cms`,
         `gatsby-plugin-sass`,
+        `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,15 @@
 import React from "react"
+import {Helmet} from "react-helmet";
 
 import Header from "./header"
 import Footer from "./footer"
 
 const Layout = ({children}) => (
     <div>
+        <Helmet>
+                <meta charSet="utf-8" />
+                <title>Habitus Capital</title>
+            </Helmet>
         <Header/>
         {children}
         <Footer/>
