d334bafbf4314817e3bafffe975009d50b3322a5
Tyron Wytrykowski <tyronwyt@gmail.com>
2018-10-11 15:40:17 +0100

    fix social icon paths

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,9 +3,9 @@ import { StaticQuery, graphql } from "gatsby"
 
 import footerStyles from "./footer.module.scss"
 import logo from "../assets/images/HabitusCapital_Logo.svg"
-// import twitter from "../assets/images/twitter_w.svg"
-// import youtube from "../assets/images/youtube_w.svg"
-// import linkedin from "../assets/images/linkedin_w.svg"
+import twitter from "../assets/images/twitter_w.svg"
+import youtube from "../assets/images/youtube_w.svg"
+import linkedin from "../assets/images/linkedin_w.svg"
 
 
 const year = new Date().getFullYear();
@@ -30,13 +30,13 @@ const Footer = () => (
                 <div className={footerStyles.blurb}>
                     <div className={footerStyles.social}>
                         <a href="https://twitter.com/HabitusCapital">
-                            {/* <img src={twitter} alt="Twitter"/> */}
+                            <img src={twitter} alt="Twitter"/>
                         </a>
                         <a href="https://youtube.com">
-                            {/* <img src={youtube} alt="Youtube"/> */}
+                            <img src={youtube} alt="Youtube"/>
                         </a>
                         <a href="https://linkedin.com">
-                            {/* <img src={linkedin} alt="Linkedin"/> */}
+                            <img src={linkedin} alt="Linkedin"/>
                         </a>
                     </div>
                     <p><strong>Address:</strong> {data.site.siteMetadata.address}<br/>
