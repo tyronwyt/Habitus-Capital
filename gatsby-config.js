30099ee6a1e3da7a157e2c1f4714d78b6941ccd9
tyronwyt <tyronwyt@gmail.com>
2018-10-16 09:37:19 +0000

    Update Sections “contact”

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,14 +16,12 @@ module.exports = {
             },
         },
         `gatsby-transformer-remark`,
-        // `gatsby-transformer-sharp`,
-        // `gatsby-plugin-sharp`,
-        // {
-        //     resolve: `gatsby-source-filesystem`,
-        //     options: {
-        //         name: `images`,
-        //         path: `${__dirname}/static/`,
-        //     }
-        // }
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `content`,
+                path: `./site/content/`,
+            },
+        },
     ],
 }
