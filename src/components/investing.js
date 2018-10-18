ec6a023ccb58b91214f1aded4602e0223ab0e411
Tyron Wytrykowski <tyronwyt@gmail.com>
2018-10-18 20:07:53 +0100

    use md data to populate blocks

--- src/components/investing.js
+++ src/components/investing.js
@@ -1,52 +1,42 @@
 import React from "react"
+import { graphql, StaticQuery } from "gatsby"
 
 import investingStyles from "./investing.module.scss"
 
-const Investing = () => (
-    <section className={investingStyles.investing}>
-        <div className="title-element">
-            <div id="investing"/>
-            <h2 className={investingStyles.title}>Investing</h2>
-        </div>
-        <div className={investingStyles.row}>
-            <div>
-                <h3>Our Edges</h3>
-                <ul>
-                <li>Our culture and environment</li>
-                <li>Our habits, processes and routines</li>
-                <li>Our automated function of evolving through failures</li>
-                <li>Our revolutionary risk management tools</li>
-                <li>Our constant search for efficiencies</li>
-                <li>Our process of extracting consistent marginal gains</li>
-                </ul>
+const Investing = ({data}) => (
+    <StaticQuery query = {graphql `
+        query {
+            block_1: markdownRemark(fileAbsolutePath: {regex: "/investing_1/"}) {
+            html
+            }
+            block_2: markdownRemark(fileAbsolutePath: {regex: "/investing_2/"}) {
+            html
+            }
+            block_3: markdownRemark(fileAbsolutePath: {regex: "/investing_3/"}) {
+                html
+            }
+            block_4: markdownRemark(fileAbsolutePath: {regex: "/investing_4/"}) {
+                html
+            }
+        }
+    `    
+    }
+
+    render = { data => (
+        <section className={investingStyles.investing}>
+            <div className="title-element">
+                <div id="investing"/>
+                <h2 className={investingStyles.title}>Investing</h2>
             </div>
-            <div>
-                <h3>Strategic Overview</h3>
-                <p>Habitus Capital has three diverse funds</p>
-                <ol>
-                    <li>Discretionary day trading with automated signalling of defined strategies</li>
-                    <li>Fully-automated algorithmic strategies</li>
-                    <li>Discretionary Swing Trading</li>
-                </ol>
-                <p>Allocation of capital is proportionately balanced between the three funds</p>
+            <div className={investingStyles.row}>
+                <div dangerouslySetInnerHTML={{__html: data.block_1.html}}></div>
+                <div dangerouslySetInnerHTML={{__html: data.block_2.html}}></div>
+                <div dangerouslySetInnerHTML={{__html: data.block_3.html}}></div>
+                <div dangerouslySetInnerHTML={{__html: data.block_4.html}} style={{ background: "#307968" }}></div>
             </div>
-            <div>
-                <h3>Management Fees</h3>
-                <p>
-                    With a focus on the future of investing, we don’t believe investors should be subject 
-                    to administration fees. By that token we believe in meriting high performance. 
-                    For this reason, we only charge a fixed percentage, performance fee on profits generated
-                </p>
-            </div>
-            <div style={{ background: "#307968" }}>
-                <h3>Raising capital</h3>
-                <p>
-                    Habitus Capital periodically invites our database of investors to review prospective investment opportunities. 
-                    Should you wish to receive a prospectus, please submit your details below
-                </p>
-            </div>
-        </div>
-    </section>
+        </section>
+        )}
+    />
 )
 
 export default Investing
