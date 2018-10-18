c9d87c4dfc8964efaeb888cac6c522cdc263baa9
Tyron Wytrykowski <tyronwyt@gmail.com>
2018-10-18 21:55:04 +0100

    remove fontawesome packages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,11 @@
 import React, { Component } from "react"
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLock } from '@fortawesome/free-solid-svg-icons'
-
-
-
-import "isomorphic-fetch"
-
 import Layout from "../components/layout"
 import Legacy from "../components/legacy"
 import Contact from "../components/contact"
 import Investing from "../components/investing"
 import Performance from "../components/performance"
 import Update from "../components/update"
-
-library.add(faLock)
 
 class IndexPage extends Component {
     constructor() {
