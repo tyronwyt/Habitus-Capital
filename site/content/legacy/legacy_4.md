bac916f130e0a58c8122dd45d782aee7e32a7dcb
Tyron Wytrykowski <tyronwyt@gmail.com>
2018-10-18 22:10:38 +0100

    populate content with content from markdown files

---
+++ site/content/legacy/legacy_4.md
@@ -0,0 +1,12 @@
+---
+date: 2018-10-18T20:38:18.689Z
+---
+### Individual Ethos
+
+<span>H</span>Humility<br/>
+<span>A</span>Accountability<br/>
+<span>B</span>Bravery<br/>
+<span>I</span>Integrity<br/>
+<span>T</span>Timely<br/>
+<span>U</span>Understanding<br/>
+<span>S</span>Selfless
--- src/components/legacy.js
+++ src/components/legacy.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql, StaticQuery } from "gatsby"
 
 import Slider from 'react-animated-slider';
 import 'react-animated-slider/build/horizontal.css';
@@ -38,78 +39,60 @@ const content = [
 ];
 
 const Legacy = () => (
-    <section className={legacyStyles.legacy}>
-        <div className="title-element">
-            <div id="top"/>  
-        </div>
-        <Slider duration="2000" autoplay="5000" touchDisabled="true" infinite="true">
-            {content.map((item, index) => <div key={index} style={{height: "100vh", background: `url('${item.image}') no-repeat center center` }}>
-                <div className={`${legacyStyles.slideContent} ${item.class}`}>
-                    <p className={legacyStyles.tense} data-tense={item.tense}>{item.tense}</p>
-                    <p className={legacyStyles.tenseDivider}><span className={legacyStyles.past}></span><span className={legacyStyles.present}></span><span className={legacyStyles.future}></span></p>
-                    <h2 className={legacyStyles.sliderTitle}>{item.title}</h2>
-                    <div className={legacyStyles.sliderDesc}>{item.description}</div>
-                </div>
-            </div>)}
-        </Slider>
-        <section className={legacyStyles.culture}>
-          <h2 className={legacyStyles.title}>Our Culture</h2>
-          <div className={legacyStyles.video}>
-                  <iframe src="https://www.youtube.com/embed/YGE5euSZnbI" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen title="Placeholder video"></iframe>
+  <StaticQuery query = {graphql `
+    query {
+        block_1: markdownRemark(fileAbsolutePath: {regex: "/legacy_1/"}) {
+          html
+        }
+        block_2: markdownRemark(fileAbsolutePath: {regex: "/legacy_2/"}) {
+          html
+        }
+        block_3: markdownRemark(fileAbsolutePath: {regex: "/legacy_3/"}) {
+            html
+        }
+        block_4: markdownRemark(fileAbsolutePath: {regex: "/legacy_4/"}) {
+          html
+      }
+    }
+  `    
+  }
+
+  render = { data => (
+      <section className={legacyStyles.legacy}>
+          <div className="title-element">
+              <div id="top"/>  
           </div>
-        </section>
-        <div className={legacyStyles.row}>
-            <div className={`${legacyStyles.block} ${legacyStyles.black} ${legacyStyles.left}`}>
-                <h3 className={legacyStyles.header}>Our DNA</h3>
-                <p>
-                    Habitus Capital mimics Natures’ truths. Unique talents co-exist within a conducive
-                    environment, striving toward efficiency and optimisation. Individual strengths are
-                    harnessed, weaknesses overcome by adding human capital, thus creating a
-                    symbiotic association. The collective Team adopts instinctive habits and smart
-                    processes that enforce a system of constant evolution. Thus, high-performance is a
-                    function of optimising the habitual processes. Through weakness we adapt, evolve
-                    and grow. Through strength, we thrive.
-                </p>
+          <Slider duration="2000" autoplay="5000" touchDisabled="true" infinite="true">
+              {content.map((item, index) => <div key={index} style={{height: "100vh", background: `url('${item.image}') no-repeat center center` }}>
+                  <div className={`${legacyStyles.slideContent} ${item.class}`}>
+                      <p className={legacyStyles.tense} data-tense={item.tense}>{item.tense}</p>
+                      <p className={legacyStyles.tenseDivider}><span className={legacyStyles.past}></span><span className={legacyStyles.present}></span><span className={legacyStyles.future}></span></p>
+                      <h2 className={legacyStyles.sliderTitle}>{item.title}</h2>
+                      <div className={legacyStyles.sliderDesc}>{item.description}</div>
+                  </div>
+              </div>)}
+          </Slider>
+          <section className={legacyStyles.culture}>
+            <h2 className={legacyStyles.title}>Our Culture</h2>
+            <div className={legacyStyles.video}>
+                    <iframe src="https://www.youtube.com/embed/YGE5euSZnbI" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen title="Placeholder video"></iframe>
             </div>
-            <div className={legacyStyles.block} style={{background: "url(" + aloe + ")", backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "none" }}>
-            </div>
-        </div>
-        <div className={legacyStyles.row}>
-            <div className={`${legacyStyles.block} ${legacyStyles.left}`} style={{background: "url(" + chameleon + ")", backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "none" }}>
-               
-            </div>
-            <div className={`${legacyStyles.block} ${legacyStyles.right}`}>
-                <h3 className={legacyStyles.header}>Our Purpose</h3>
-                <p>
-                    To <strong>build</strong> an <strong>exclusive team</strong> of <strong>talented individuals</strong> that share a common <strong>ideology</strong> and create a <strong>unique cultural identity</strong> through practiced habits.
-                </p>
-            </div>
-        </div>
-        <div className={legacyStyles.row}>
-            <div className={`${legacyStyles.block} ${legacyStyles.left}`} style={{ background: "#307968", color: "#fff" }}>
-                <h3 className={legacyStyles.header}>Vision Statement</h3>
-                <p>
-                    Our Cultural Model will become world renowned and our practices adopted globally.<br/>
-                    Our Unique Environment will be designed and built by the team, for the team.<br/>
-                    We will Create tools that unlock true human potential, leading to world class performers.<br/>
-                    We will Re-invent the manner in which profits are extracted from financial markets.<br/>
-                    We will Outperform Constantly, Consistently and Ethically.
-                </p>
-            </div>
-            <div className={`${legacyStyles.block} ${legacyStyles.right}`}  style={{ background: "#000", color: "#fff" }}>
-                <h3 className={legacyStyles.header}>Individual Ethos</h3>
-                <p>
-                    <span className={legacyStyles.highlight}>H</span>Humility<br/>
-                    <span className={legacyStyles.highlight}>A</span>Accountability<br/>
-                    <span className={legacyStyles.highlight}>B</span>Bravery<br/>
-                    <span className={legacyStyles.highlight}>I</span>Integrity<br/>
-                    <span className={legacyStyles.highlight}>T</span>Timely<br/>
-                    <span className={legacyStyles.highlight}>U</span>Understanding<br/>
-                    <span className={legacyStyles.highlight}>S</span>Selfless
-                </p>
-            </div>
-        </div>
-    </section>
+          </section>
+          <div className={legacyStyles.row}>
+              <div className={`${legacyStyles.block} ${legacyStyles.black} ${legacyStyles.left}`} dangerouslySetInnerHTML={{__html: data.block_1.html}}></div>
+              <div className={legacyStyles.block} style={{background: "url(" + aloe + ")", backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "none" }}></div>
+          </div>
+          <div className={legacyStyles.row}>
+              <div className={`${legacyStyles.block} ${legacyStyles.left}`} style={{background: "url(" + chameleon + ")", backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "none" }}></div>
+              <div className={`${legacyStyles.block} ${legacyStyles.right}`} dangerouslySetInnerHTML={{__html: data.block_2.html}}></div>
+          </div>
+          <div className={legacyStyles.row}>
+              <div className={`${legacyStyles.block} ${legacyStyles.left}`} style={{ background: "#307968", color: "#fff" }} dangerouslySetInnerHTML={{__html: data.block_3.html}}></div>
+              <div className={`${legacyStyles.block} ${legacyStyles.right} ${legacyStyles.highlight}`} style={{ background: "#000", color: "#fff" }} dangerouslySetInnerHTML={{__html: data.block_4.html}}></div>
+          </div>
+      </section>
+    )}
+  />
 )
 
 export default Legacy
