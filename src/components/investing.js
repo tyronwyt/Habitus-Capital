a6e3ce5d351efdafec27f5d19e7c909cc6ce7825
Tyron Wytrykowski <tyronwyt@gmail.com>
2018-10-19 15:03:22 +0100

    add investor contact form modal

--- src/components/investing.js
+++ src/components/investing.js
@@ -1,42 +1,85 @@
-import React from "react"
+import React, { Component } from "react"
 import { graphql, StaticQuery } from "gatsby"
 
+import InvestorForm from "./investorForm"
 import investingStyles from "./investing.module.scss"
 
-const Investing = ({data}) => (
-    <StaticQuery query = {graphql `
-        query {
-            block_1: markdownRemark(fileAbsolutePath: {regex: "/investing_1/"}) {
-            html
-            }
-            block_2: markdownRemark(fileAbsolutePath: {regex: "/investing_2/"}) {
-            html
-            }
-            block_3: markdownRemark(fileAbsolutePath: {regex: "/investing_3/"}) {
-                html
-            }
-            block_4: markdownRemark(fileAbsolutePath: {regex: "/investing_4/"}) {
-                html
-            }
+class Investing extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            modalOpen: false, 
         }
-    `    
+
+        this.modalHandler = this.modalHandler.bind(this);
+        this.onClickHandler = this.onClickHandler.bind(this);
     }
 
-    render = { data => (
-        <section className={investingStyles.investing}>
-            <div className="title-element">
-                <div id="investing"/>
-                <h2 className={investingStyles.title}>Investing</h2>
-            </div>
-            <div className={investingStyles.row}>
-                <div dangerouslySetInnerHTML={{__html: data.block_1.html}}></div>
-                <div dangerouslySetInnerHTML={{__html: data.block_2.html}}></div>
-                <div dangerouslySetInnerHTML={{__html: data.block_3.html}}></div>
-                <div dangerouslySetInnerHTML={{__html: data.block_4.html}} style={{ background: "#307968" }}></div>
-            </div>
-        </section>
-        )}
-    />
-)
+    modalHandler() {
+        this.setState({modalOpen: false});
+    }
+    onClickHandler() {
+        this.setState({modalOpen: true});
+    }
+
+    showModal() {
+        
+        // Prevent scrolling when modal is open
+        // const body = document.getElementsByTagName('body');
+
+        if (this.state.modalOpen) {
+               
+            return (
+                <InvestorForm onModalClose={this.modalHandler}/>
+            )
+        } 
+        // else {
+        //     const body = document.getElementsByTagName('body');
+        //     body[0].setAttribute('style', 'overflow: hidden');
+        // }
+    }
+
+    render() {
+        return (
+            <StaticQuery query = {graphql `
+            query {
+                block_1: markdownRemark(fileAbsolutePath: {regex: "/investing_1/"}) {
+                html
+                }
+                block_2: markdownRemark(fileAbsolutePath: {regex: "/investing_2/"}) {
+                html
+                }
+                block_3: markdownRemark(fileAbsolutePath: {regex: "/investing_3/"}) {
+                    html
+                }
+                block_4: markdownRemark(fileAbsolutePath: {regex: "/investing_4/"}) {
+                    html
+                }
+            }
+        `    
+        }
+    
+        render = { data => (
+            <section className={investingStyles.investing}>
+                <div className="title-element">
+                    <div id="investing"/>
+                    <h2 className={investingStyles.title}>Investing</h2>
+                </div>
+                <div className={investingStyles.row}>
+                    <div dangerouslySetInnerHTML={{__html: data.block_1.html}}></div>
+                    <div dangerouslySetInnerHTML={{__html: data.block_2.html}}></div>
+                    <div dangerouslySetInnerHTML={{__html: data.block_3.html}}></div>
+                    <div style={{ background: "#307968" }}>
+                        <div dangerouslySetInnerHTML={{__html: data.block_4.html}} style={{padding: 0, background: "none"}}></div>
+                        <div className={investingStyles.button} onClick={this.onClickHandler}>Investor Relations</div>
+                    </div>
+                </div>
+                {this.showModal()}
+            </section>
+            )}
+        />
+        )
+    }
+}
 
 export default Investing
---
+++ src/components/investorForm.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react"
+
+import investorStyles from "./investorForm.module.scss"
+
+class InvestorForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.modalHandler = this.modalHandler.bind(this);
+    }
+
+    modalHandler() {
+        this.props.onModalClose();
+    }
+
+
+    render() {
+        return (
+            <div className={investorStyles.modal}>
+            <div className={investorStyles.background } 
+            onClick={this.modalHandler}
+            ></div>
+            <div className={investorStyles.formWrapper}>
+                <form name="contact" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact" />
+                    <h3>Request a prospectus</h3>
+                    <p>
+                        <input type="text" name="first_name" placeholder="First Name"/>
+                    </p>
+                    <p>
+                        <input type="text" name="last_name" placeholder="Last Name"/>
+                    </p>
+                    <p>
+                        <input type="email" name="email" placeholder="Email"/>
+                    </p>
+                    <p>
+                        <label><input type="checkbox" name="accept_terms"/> I accept the terms and conditions</label>
+                    </p>
+                    <p>
+                        <button type="submit">Submit</button>
+                    </p>
+                </form>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default InvestorForm
+
