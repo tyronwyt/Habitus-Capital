9a66aba545504943905a5fa137b128bddd08199d
Tyron Wytrykowski <tyronwyt@gmail.com>
2018-10-25 16:51:37 +0100

    add hidden form for netlify cms

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,6 +16,25 @@ const Layout = ({children}) => (
         <Header/>
         {children}
         <Footer/>
+        <form name="contact" method="POST" data-netlify="true" style={{display: "none"}}>
+                <input type="hidden" name="form-name" value="contact" />
+                    <h3>Request a prospectus</h3>
+                    <p>
+                        <input type="text" name="first_name" placeholder="First Name"/>
+                    </p>
+                    <p>
+                        <input type="text" name="last_name" placeholder="Last Name"/>
+                    </p>
+                    <p>
+                        <input type="email" name="email" placeholder="Email"/>
+                    </p>
+                    <p>
+                        <label><input type="checkbox" name="accept_terms"/> I accept the terms and conditions</label>
+                    </p>
+                    <p>
+                        <button type="submit">Submit</button>
+                    </p>
+        </form>
     </div>
 )
 
