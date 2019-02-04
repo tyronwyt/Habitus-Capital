aa304a01a9460f984b68d4fdb2bcc27e363fbec7
Tyron Wytrykowski <tyronwyt@gmail.com>
2019-02-04 22:14:41 +0000

    imporve spreadsheet fetch performance and cache loaded sheets

--- src/components/performance.js
+++ src/components/performance.js
@@ -10,69 +10,56 @@ class Performance extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            chartData: [],
             selectedFundKey: "0",
             selectedFund: "",
             loaded: false,
         };
 
         this.setActive = this.setActive.bind(this);
+        this.fetchChartData();
     }
 
-    componentDidMount() {
-        
-        const firstFund = document.getElementById('first_fund');
-        const selectedFund = firstFund.innerHTML
-        this.setState({selectedFund: selectedFund}, () => {this.fetchChartData();})
-    }
+    chartData = [];
+    feedUrls = [
+        'https://spreadsheets.google.com/feeds/list/1UAhYsgzm2Qy4iuOx1hJt8RgMtjP1jaGmUvCid-oISAE/od6/public/basic?alt=json',
+        'https://spreadsheets.google.com/feeds/list/1NZM91sYM446Hry0H7stMnlalj4eCc-yAgDjF_TCMqrI/od6/public/basic?alt=json',
+        'https://spreadsheets.google.com/feeds/list/1969fIH-xw5DGl6HEexaX4orUyo4Q-fFVXaGggZJJBDA/od6/public/basic?alt=json'
+    ]
 
     fetchChartData() {
-        const feedUrls = [
-            'https://spreadsheets.google.com/feeds/list/1UAhYsgzm2Qy4iuOx1hJt8RgMtjP1jaGmUvCid-oISAE/od6/public/basic?alt=json',
-            'https://spreadsheets.google.com/feeds/list/1NZM91sYM446Hry0H7stMnlalj4eCc-yAgDjF_TCMqrI/od6/public/basic?alt=json',
-            'https://spreadsheets.google.com/feeds/list/1969fIH-xw5DGl6HEexaX4orUyo4Q-fFVXaGggZJJBDA/od6/public/basic?alt=json'
-        ]
-        const selectedUrl = () => {
-            switch(this.state.selectedFundKey) {
-                case "0":
-                    return feedUrls[0];
-                case "1":
-                    return feedUrls[1];
-                case "2":
-                    return feedUrls[2];
-                default:
-                    return null
-            }
-        }
-        fetch(selectedUrl())
-        .then(response => {
-            return response.json();
-        }).then( response => {
-            const data = response.feed.entry,
-            labels = [],
-            dataset = [];
-            for (let i = 0; i < data.length; i++) {
-                labels.push(data[i].title.$t);
-                dataset.push(data[i].content.$t.replace('value: ', ''));
-            }
-            let responseData = {
-                labels: labels,
-                datasets: [
-                    {
-                    label: "% Return on Capital m/m ",
-                    data: dataset,
-                    backgroundColor: "#307968",
-                    }
-                ]
-            }
-            this.setState({chartData: responseData, loaded: true});
-        })
+        if (this.chartData[this.state.selectedFundKey] == null) {
+            fetch(this.feedUrls[this.state.selectedFundKey])
+            .then(response => {
+                return response.json()
+            })
+            .then(response => {
+                const data = response.feed.entry,
+                labels = [],
+                dataset = [];
+                for (let i = 0; i < data.length; i++) {
+                    labels.push(data[i].title.$t);
+                    dataset.push(data[i].content.$t.replace('value: ', ''));
+                }
+                let responseData = {
+                    labels: labels,
+                    datasets: [
+                        {
+                        label: "% Return on Capital m/m ",
+                        data: dataset,
+                        backgroundColor: "#307968",
+                        }
+                    ]
+                }
+                this.chartData[this.state.selectedFundKey] = responseData;
+                this.setState({loaded: true});
+            })
+        } else { this.setState({loaded: true}); }      
     }
 
     chartContent() {
         return (
             <HorizontalBar height={100}
-                data={this.state.chartData}
+                data={this.chartData[this.state.selectedFundKey]}
                 options={{
                     scales: {
                         xAxes: [{
